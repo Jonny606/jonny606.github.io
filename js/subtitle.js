dbd6a35ecbae8862cedd5843ad0417d2c657b62b
Jonny606 <yosyas607@gmail.com>
2025-09-17 15:22:42 -0400

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -4,7 +4,7 @@ function getRandomGreeting() {
     "Nijuru IS THE GOAT",
     "This website was too easy to make",
      "Computer Science is the best Subject",
-     "Brawl Stars is the best game",
+     "Brawl Stars is the best game"
   ];
   var randomIndex = Math.floor(Math.random() * greetings.length);
   return greetings[randomIndex];
