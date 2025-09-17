696c2aff1b7ade5762823ad00361ff99e7b1d5cd
Jonny606 <yosyas607@gmail.com>
2025-09-17 15:19:24 -0400

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -1,10 +1,10 @@
 function getRandomGreeting() {
   var greetings = [
     "Beep boop! Are you a robot?",
-    "Nijuru IS THE GOAT"
-    "This website was too easy to make"
-     "Computer Science is the best Subject"
-     "Brawl Stars is the best game"
+    "Nijuru IS THE GOAT",
+    "This website was too easy to make",
+     "Computer Science is the best Subject",
+     "Brawl Stars is the best game",
   ];
   var randomIndex = Math.floor(Math.random() * greetings.length);
   return greetings[randomIndex];
