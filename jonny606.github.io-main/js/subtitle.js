2dc7a595fe6adef7cb33181e6b1160b81711bbf4
Jonny606 <yosyas607@gmail.com>
2025-11-26 14:25:29 -0500

    Add Firebase workflow configuration

---
+++ jonny606.github.io-main/js/subtitle.js
@@ -0,0 +1,13 @@
+function getRandomGreeting() {
+  var greetings = [
+    "Beep boop! Are you a robot?",
+    "Nijuru IS THE GOAT",
+    "This website was too easy to make",
+     "Computer Science is the best Subject",
+     "Brawl Stars is the best game"
+  ];
+  var randomIndex = Math.floor(Math.random() * greetings.length);
+  return greetings[randomIndex];
+}
+
+document.getElementById("subtitle").innerText = getRandomGreeting();
